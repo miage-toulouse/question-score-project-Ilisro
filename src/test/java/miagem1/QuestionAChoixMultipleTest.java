310de28548499ac927a3fa1d5efddfb09cbd80ec
Ilisro <michael.herve12@gmail.com>
2019-11-29 14:19:56 +0100

    @Test

--- src/test/java/miagem1/QuestionAChoixMultipleTest.java
+++ src/test/java/miagem1/QuestionAChoixMultipleTest.java
@@ -1,45 +1,54 @@
 package miagem1;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
-public class QuestionAChoixExclusifTest {
+public class QuestionAChoixMultipleTest {
 
-    private QuestionAChoixExclusif questionE;
+    private QuestionAChoixMultiple questionM;
 
     @Before
     public void setUp() throws Exception {
         //Given : une instance de question à choix exclusif
-        questionE = new QuestionAChoixExclusif("Quel est la différence entre un pigeon ?", 2);
+        ArrayList<Integer> bonnesReponses = new ArrayList<Integer>();
+        bonnesReponses.add(2);
+        bonnesReponses.add(3);
+        questionM = new QuestionAChoixMultiple("Quel est la différence entre un canard ?", bonnesReponses);
     }
 
     @Test
     public void testGetEnonce() {
         //When : on demande son énoncé à la question
-        String res = questionE.getEnonce();
+        String res = questionM.getEnonce();
         //Then : l'énoncé retourné est l'énoncé fourni à la construction de la question
-        assertEquals("Quel est la différence entre un pigeon ?", res);
+        assertEquals("Quel est la différence entre un canard ?", res);
     }
 
     @Test
-    public void testGetScoreForIndiceBonneReponse() {
+    public void testGetScoreForIndiceBonnesRéponses() {
         //When : on demande l'indice de la réponse
         int indiceEtu = 2;
         //And : on demande le score de l'indice à la question
-        float resScore = questionE.getScoreForIndice(indiceEtu);
+        float resScore = questionM.getScoreForIndice(indiceEtu);
+
         //Then : le score obtenu est 100
-        assertEquals(100f, resScore, 0.01f);
+        assertEquals(50f, resScore, 0.01f);
     }
 
     @Test
-    public void testGetScoreForIndiceMauvaiseReponse() {
+    public void testGetScoreForIndiceMauvaiseReponses() {
         //When : on demande l'indice de la réponse
         int indiceEtu = 1;
         //And : on demande le score de l'indice à la question
-        float resScore = questionE.getScoreForIndice(indiceEtu);
+        float resScore = questionM.getScoreForIndice(indiceEtu);
         //Then : le score obtenu est 0
         assertEquals(0f, resScore, 0.01f);
     }
+
 }
